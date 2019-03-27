4b1d82ed658c5737792d3efdbeba527ceab83324
gurukamal <info.gurukamalchawla@gmail.com>
2019-03-27 22:02:30 +0530

    edited app component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <p>This is using of git hub.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
